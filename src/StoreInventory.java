37a85ad58df0f78220ff24e6c4caa55520a79022
Atheek Ashkar <aashkar2017@fau.edu>
2023-10-28 12:37:05 -0400

    Worked on some methods for StoreInventory.java

--- src/StoreInventory.java
+++ src/StoreInventory.java
@@ -13,10 +13,27 @@ public class StoreInventory {
         this.inventory = inventory;
     }
     public void addProduct(Product p){
-        this.inventory.add(p);
+        if(!this.inventory.contains(p.getID())){
+            this.inventory.add(p);
+        }else{
+            System.out.println("Product already exists");
+        }
+
     }
 
-    public void sellProduct(Product p, int q){
-
+    public void sellProduct(String ID, int q){
+        for(Product p: this.inventory){
+            if(p.getID().equals(ID)){
+                p.sell(q);
+            }
+        }
+    }
+    public Product getProduct(String productId) {
+        for (Product product : this.inventory) {
+            if (product.getID().equals(productId)) {
+                return product;
+            }
+        }
+        return null;
     }
 }
