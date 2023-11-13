88ddd2eb20d028a743ee4c37e4394c2b303fbbe5
blueswizzle <83927505+blueswizzle@users.noreply.github.com>
2023-11-13 11:08:52 -0500

    Added costs,revenues, and profits variables to Seller.java

--- src/users_code/Seller.java
+++ src/users_code/Seller.java
@@ -5,7 +5,11 @@ import src.Inventory.Product;
 import java.util.ArrayList;
 
 public class Seller extends User{
-    ArrayList<Product> sellerProducts;
+    private ArrayList<Product> sellerProducts;
+    private double costs;
+    private double revenues;
+    private double profits;
+
     public Seller(String firstName, String lastName, String username, String password, String email) {
         super(firstName, lastName, username, password, email);
         this.sellerProducts = new ArrayList<>();
