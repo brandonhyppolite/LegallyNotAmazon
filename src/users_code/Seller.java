ee4579e91e6c4e15a5271f0e8a19b4d491e50c99
blueswizzle <83927505+blueswizzle@users.noreply.github.com>
2023-11-13 10:32:35 -0500

    removed addProductTesting() method from Seller.java. New Sellers will now start with no products to sell and have to add them manually

--- src/users_code/Seller.java
+++ src/users_code/Seller.java
@@ -9,19 +9,6 @@ public class Seller extends User{
     public Seller(String firstName, String lastName, String username, String password, String email) {
         super(firstName, lastName, username, password, email);
         this.sellerProducts = new ArrayList<>();
-        addProductsTesting();
-    }
-
-    private void addProductsTesting(){
-        for (int i = 1; i <= 30; i++) {
-            String productName = "Product" + i;
-            double productInvoicePrice = i * 9.5;
-            double productPrice = 10.0 * i; // Replace this with your desired logic for price
-            int productQuantity = 20; // Replace this with your desired quantity
-
-            Product product = new Product(productName, productInvoicePrice,productPrice, productQuantity);
-            sellerProducts.add(product);
-        }
     }
 
     public int getProductSize(){
