65615bdcfed522daa41a4aca557bf0556529d60e
Atheek Ashkar <aashkar2017@fau.edu>
2023-11-12 21:00:47 -0500

    Some minor edits

--- src/GUI_code/ShoppingCartApp.java
+++ src/GUI_code/ShoppingCartApp.java
@@ -5,11 +5,9 @@ import src.Backend.ShoppingSystem;
 import javax.swing.*;
 
 public class ShoppingCartApp extends JFrame {
-    private ShoppingSystem system;
     private ViewManager viewManager;
 
     public ShoppingCartApp() {
-        this.system = ShoppingSystem.getInstance();
         this.setTitle("Not Amazon");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(800, 600);
