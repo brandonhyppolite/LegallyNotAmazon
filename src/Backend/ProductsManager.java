3690d3ddfc45cbf0285f1952192c9a4e4a5a1adb
AGitHubMan2023 <mjimenez2019@fau.edu>
2023-12-04 20:29:06 -0500

    logout for buyer

--- src/Backend/ProductsManager.java
+++ src/Backend/ProductsManager.java
@@ -4,7 +4,6 @@ import src.Product.Product;
 import src.users_code.Buyer;
 import src.users_code.Seller;
 import src.users_code.User;
-
 import java.util.ArrayList;
 
 /**
@@ -12,6 +11,9 @@ import java.util.ArrayList;
  * setting up lists of sellers and buyers, and adding products to users.
  */
 public class ProductsManager {
+    /**
+     * The `UserManager` instance that this `ProductsManager` is associated with.
+     */
     private final UserManager userManager;
 
 
@@ -43,7 +45,11 @@ public class ProductsManager {
         ProductFileHandler.writeInventoryToFile(this.userManager);
     }
 
-
+    /**
+     * Gets all products that are for sale.
+     *
+     * @return An `ArrayList` of all products that are for sale.
+     */
     public ArrayList<Product> getAllItemsForSale() {
         ArrayList<Product> allItemsForSale = new ArrayList<>();
 
@@ -55,7 +61,12 @@ public class ProductsManager {
 
         return allItemsForSale;
     }
-
+    /**
+     * Gets all products that match the given search phrase.
+     *
+     * @param searchPhrase The search phrase to match products against.
+     * @return An `ArrayList` of all products that match the given search phrase.
+     */
     public ArrayList<Product> getAvailableItems(String searchPhrase) {
         ArrayList<Product> availableItems = new ArrayList<>();
 
@@ -71,7 +82,13 @@ public class ProductsManager {
 
         return availableItems;
     }
-
+    /**
+     * Gets the current quantity of a product in a buyer's shopping cart.
+     *
+     * @param buyer  The buyer whose shopping cart to check.
+     * @param product The product to check the quantity of.
+     * @return The current quantity of the product in the buyer's shopping cart.
+     */
     public int getCurrentQuantityOfProductsInCart(Buyer buyer, Product product){
         int count = 0;
         for(Product p: buyer.getShoppingCart()){
@@ -81,7 +98,12 @@ public class ProductsManager {
         }
         return count;
     }
-
+    /**
+     * Gets a product by its ID.
+     *
+     * @param id The ID of the product to get.
+     * @return The product with the given ID, or `null` if no product with the given ID exists.
+     */
     public Product getProductByID(String id){
         for(User u: this.userManager.getUsers()){
             if(u instanceof Seller){
@@ -94,7 +116,12 @@ public class ProductsManager {
         }
         return null;
     }
-
+    /**
+     * Gets the seller who owns a product by its ID.
+     *
+     * @param id The ID of the product to get the seller of.
+     * @return The seller who owns the product with the given ID, or `null` if no product with the given ID exists.
+     */
     public Seller getSellerFromProductID(String id){
         for(User u: this.userManager.getUsers()){
             if(u instanceof Seller){
